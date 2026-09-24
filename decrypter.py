0a10b1283a1d658d05529f3fe4469db954f8be38
Tainara <tainaragomes.nsg@gmail.com>
2026-09-24 16:59:37 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -3,5 +3,5 @@ from cryptography.fernet import Fernet
 chave = open("chave.key", "rb").read()            
 arquivo = open("arquivo_criptografado.txt", "rb").read()
 arquivo = Fernet(chave).decrypt(arquivo)
-open("arquivo_restaurado.txt", "mb").write(arquivo)
+open("arquivo_restaurado.txt", "wb").write(arquivo)
 print("Arquivo Restaurado!")
